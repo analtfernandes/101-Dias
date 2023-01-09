3135e5b0df3f2f7558d351b1ebba3c0c1f439e70
Ana Fernandes <analuizateixeirafernandes184@gmail.com>
2023-01-09 15:04:48 -0300

    fix: fade props

--- src/components/utils/TransitionFade.js
+++ src/components/utils/TransitionFade.js
@@ -50,20 +50,25 @@ export function TransitionFade({ fadeConfig, setFadeConfig }) {
 	}
 	if (fadeConfig.await && fadeConfig.custom) setCustomFade();
 
-	return <Fade visible={fadeConfig.isVisible} display={fadeConfig.display} />;
+	return (
+		<Fade
+			visible={fadeConfig.isVisible ? "1" : "0"}
+			display={fadeConfig.display ? "initial" : "none"}
+		/>
+	);
 }
 
 const Fade = styled.div`
 	width: 100%;
 	height: 100%;
 	background-color: black;
-	opacity: ${(props) => (props.visible ? "1" : "0")};
+	opacity: ${(props) => props.visible};
 	position: fixed;
 	z-index: 2;
 	top: 0;
 	bottom: 0;
 	left: 0;
 	right: 0;
-	display: ${(props) => (props.display ? "initial" : "none")};
+	display: ${(props) => props.display};
 	transition: all 1.5s linear;
 `;
