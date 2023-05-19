17d0fda4981864fd40f1331b573be623d98b47a0
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-05-19 12:29:41 -0300

    refactor: add format helper in database

---
+++ src/database/formatHelper.js
@@ -0,0 +1,23 @@
+function freezeMap(map, collectionName = null) {
+	const errorMessage = collectionName
+		? `Object ${collectionName} is frozen!`
+		: "Object is frozen!";
+
+	const action = () => {
+		throw new Error(errorMessage);
+	};
+
+	map.set = action;
+	map.delete = action;
+	map.clear = action;
+}
+
+function map(data, collectionName) {
+	return freezeMap(new Map(data), collectionName);
+}
+
+const format = {
+	map,
+};
+
+export { format };
