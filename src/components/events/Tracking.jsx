2102e005c9d1080b64e35977fc61a3ed7b0ddac5
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-06-07 16:58:28 -0300

    feat: save game every 4 hours

--- src/components/events/Tracking.jsx
+++ src/components/events/Tracking.jsx
@@ -2,25 +2,33 @@ import { useEffect, useState, useRef } from "react";
 import { useStatusContext } from "../../contexts";
 import { choiceEvents, events } from "../../database/events.js";
 import { MODAL_TYPES } from "../../enums";
+import { useSave } from "../../hooks";
 
 import { Modal } from "./Modal.jsx";
 
 export function Tracking() {
-	const { status } = useStatusContext();
 	const [modalConfig, setModalConfig] = useState({ isOpen: false, type: "" });
 	const eventData = useRef({});
+	const { status } = useStatusContext();
+	const saveGame = useSave();
 
 	useEffect(() => {
-		if (!events[status.day] && !choiceEvents[status.day]) return;
+		const HOUR_FOUR = 240;
+		const day = status.day;
+		const time = status.time;
+	
+		if (time % HOUR_FOUR === 0) saveGame();
+	
+		if (!events[day] && !choiceEvents[day]) return;
 
-		eventData.current = choiceEvents[status.day][status.time];
+		eventData.current = choiceEvents[day][time];
 
 		if (eventData.current) {
 			setModalConfig({ isOpen: true, type: MODAL_TYPES.choiceEvent });
 			return;
 		}
 
-		eventData.current = events[status.day][status.time];
+		eventData.current = events[day][time];
 
 		if (eventData.current) {
 			setModalConfig({ isOpen: true, type: MODAL_TYPES.event });
